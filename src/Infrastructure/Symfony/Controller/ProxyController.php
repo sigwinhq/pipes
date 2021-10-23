7426c7fa8b237b66ba6ff356e4433ed824d8ce78
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2021-10-23 14:34:24 +0200

    Proxy (#1)
    * Proxy WIP
    
    * Proxy works

---
+++ src/Infrastructure/Symfony/Controller/ProxyController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Infrastructure\Symfony\Controller;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProxyController extends AbstractController
+{
+    private ClientInterface $httpClient;
+    private RequestFactoryInterface $requestFactory;
+    private HttpFoundationFactory $foundationFactory;
+
+    public function __construct(ClientInterface $httpClient, RequestFactoryInterface $requestFactory, HttpFoundationFactory $foundationFactory)
+    {
+        $this->httpClient = $httpClient;
+        $this->requestFactory = $requestFactory;
+        $this->foundationFactory = $foundationFactory;
+    }
+    
+    /**
+     * @Route("/proxy/{uri}", name="proxy", requirements={"uri": ".+"})
+     */
+    public function __invoke(Request $currentRequest, string $uri): Response
+    {
+        $request = $this->requestFactory->createRequest($currentRequest->getMethod(), $uri);
+        $response = $this->httpClient->sendRequest($request);
+
+        return $this->foundationFactory->createResponse($response);
+    }
+}
